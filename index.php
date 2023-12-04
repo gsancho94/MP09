aa8399e6ee078cb5d5366683345bb035b3a4376d
gsancho94 <gsancho@iesebre.com>
2023-12-04 19:54:35 +0100

    Sync

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
+<<<<<<< HEAD
 
 require  "/app/Index.php";
 require  "/resources/viwes/index.blade.php";
 
+=======
+//echo 'Hello World!';
+
+require 'app/index.php';
+
+require 'resources/views/index.blade.php';
+>>>>>>> 1cff614 (Sync)
